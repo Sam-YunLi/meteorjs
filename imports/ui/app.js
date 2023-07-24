f04bbbacb4f034e8679f2a0340d493b33702ee0b
Yun-Li <sam.looong@gmail.com>
2023-07-24 22:17:07 +1000

    li

--- imports/ui/app.js
+++ imports/ui/app.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const App = () => {
   return (
+    <>
     <div>Hello world</div>
+    <p>List of the day of working</p>
+    <li>finish the work</li>
+    <li>go to the gym</li>
+    <li>eat well</li>
+    <li>sleep well</li>
+    </>
   )
 }
 
